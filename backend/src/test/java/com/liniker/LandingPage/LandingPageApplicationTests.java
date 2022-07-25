854ee0469940eaf63006410a36be61ec9a3c5cbc
LinikerNogueiraMachado <linikernogueiramachado@gmail.com>
2022-07-25 12:57:19 -0300

    Inicio do projeto

---
+++ backend/src/test/java/com/liniker/LandingPage/LandingPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liniker.LandingPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
