00c43d652eb604117bda7165ed08628040b9a981
LinikerNogueiraMachado <linikernogueiramachado@gmail.com>
2022-07-26 00:33:42 -0300

    Component Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,18 +1,28 @@
-import ReactFilmesIcon from 'assets/img/icons/ReactFilmesIcon.png';
 import FilmesGraph from 'assets/img/FilmeGraph.png';
+import './style.css'
 
 function Navbar() {
     return (
-        <header>
-            <nav>
-                <div><img src={FilmesGraph} />
-                </div>
-                <h1>React Filmes</h1>
-                <div>
-                    <img src={ReactFilmesIcon} />
-                </div>
-            </nav>
-        </header>
+        <>
+            <header>
+                <nav className='container'>
+                    <div className='page-nav-content'>
+                        <h1>React Filmes</h1>
+                        <ul>
+                            <h2> Filmes por gênero </h2>
+                            <li> <a href="/"> Ação         </a></li>
+                            <li> <a href="/"> Aventura     </a></li>
+                            <li> <a href="/"> Comédia      </a></li>
+                            <li> <a href="/"> Documentário </a></li>
+                            <li> <a href="/"> Drama        </a></li>
+                            <li> <a href="/"> Ficção       </a></li>
+                            <li> <a href="/"> Romance      </a></li>
+                            <li> <a href="/"> Terror       </a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
     );
 }
 
