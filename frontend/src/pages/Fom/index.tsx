1e49b68d12cb861a74c704b0579ecb9b69752317
LinikerNogueiraMachado <linikernogueiramachado@gmail.com>
2022-07-26 22:46:56 -0300

    Layout da pagina de Formulario

--- frontend/src/pages/Fom/index.tsx
+++ frontend/src/pages/Fom/index.tsx
@@ -1,7 +1,44 @@
+import './style.css';
+
+// ======== ======== declarando objeto ======= ======= //
+const movie = {
+    id: 1,
+    image: " https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5
+};
+
 function Form() {
 
     return (
-        <h1>Pagina de formulário</h1>
+
+        <div className="form__container">
+            <img className="card__image--movie" src={movie.image} alt={movie.title} />
+            <div className="card__container--bottom">
+                <h3>{movie.title}</h3>
+                <form className="form--LandingPage">
+                    <div className="form__group form__group--LandingPage">
+                        <label htmlFor="email">Informe seu e-mail: </label>
+                        <input type="email" className="form__control" id="email" />
+                    </div>
+                    <div className="form__group form__group--LandingPage">
+                        <label htmlFor="score">Qual a sua avaliação? </label>
+                        <select className="form__control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="form__container--btn">
+                        <button type="submit" className="btn btn--primary LandingPage--btn">Salvar</button>
+                    </div>
+                </form>
+                <button className="btn btn--primary LandingPage--btn mt-3">Cancelar</button>
+            </div>
+        </div>
     );
 }
 
